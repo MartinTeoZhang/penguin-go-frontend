6843b08f51ba26e0de5f9095cd60428840dc92cb
Ryzin <914571420@qq.com>
2020-01-28 21:58:26 +0800

    国际化

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 import 'font-awesome/css/font-awesome.min.css'
 import api from './http/index' // 在 main.js 中以 vue 插件的形式引入 axios，
 // 这样在其他地方就可通过 this.$api 调用相关的接口了。
+import i18n from './i18n'
 
 Vue.use(ElementUI) // 导入所有Element-UI组件
 Vue.use(api) // 导入 axios 插件
@@ -16,6 +17,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  i18n,
   router,
   components: { App },
   template: '<App/>'
