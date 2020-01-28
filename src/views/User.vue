4370884efff4d7335bbfa592ccb2c68d35c8af95
Ryzin <914571420@qq.com>
2020-01-28 17:31:14 +0800

    解决导航菜单点击之后不能再次点击的问题\n 添加机构、角色、日志管理等菜单

--- src/views/User.vue
+++ src/views/User.vue
@@ -16,46 +16,47 @@
   }
 </script>
 
-<style lang="scss">
-  .site-wrapper.site-page--not-found {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    overflow: hidden;
-    .site-content__wrapper {
-      padding: 0;
-      margin: 0;
-      background-color: #fff;
-    }
-    .site-content {
-      position: fixed;
-      top: 15%;
-      left: 50%;
-      z-index: 2;
-      padding: 30px;
-      text-align: center;
-      transform: translate(-50%, 0);
-    }
-    .not-found-title {
-      margin: 20px 0 15px;
-      font-size: 8em;
-      font-weight: 500;
-      color: rgb(55, 71, 79);
-    }
-    .not-found-desc {
-      margin: 0 0 30px;
-      font-size: 26px;
-      text-transform: uppercase;
-      color: rgb(118, 131, 143);
-      > em {
-        font-style: normal;
-        color: #ee8145;
-      }
-    }
-    .not-found-btn-gohome {
-      margin-left: 30px;
-    }
-  }
-</style>
+<!--这里发现点击导航菜单之后，菜单就不能点击了，经查看是导航页面坐标样式问题，把用户管理等页面的样式去掉就好了。-->
+<!--<style lang="scss">-->
+<!--  .site-wrapper.site-page&#45;&#45;not-found {-->
+<!--    position: absolute;-->
+<!--    top: 0;-->
+<!--    right: 0;-->
+<!--    bottom: 0;-->
+<!--    left: 0;-->
+<!--    overflow: hidden;-->
+<!--    .site-content__wrapper {-->
+<!--      padding: 0;-->
+<!--      margin: 0;-->
+<!--      background-color: #fff;-->
+<!--    }-->
+<!--    .site-content {-->
+<!--      position: fixed;-->
+<!--      top: 15%;-->
+<!--      left: 50%;-->
+<!--      z-index: 2;-->
+<!--      padding: 30px;-->
+<!--      text-align: center;-->
+<!--      transform: translate(-50%, 0);-->
+<!--    }-->
+<!--    .not-found-title {-->
+<!--      margin: 20px 0 15px;-->
+<!--      font-size: 8em;-->
+<!--      font-weight: 500;-->
+<!--      color: rgb(55, 71, 79);-->
+<!--    }-->
+<!--    .not-found-desc {-->
+<!--      margin: 0 0 30px;-->
+<!--      font-size: 26px;-->
+<!--      text-transform: uppercase;-->
+<!--      color: rgb(118, 131, 143);-->
+<!--      > em {-->
+<!--        font-style: normal;-->
+<!--        color: #ee8145;-->
+<!--      }-->
+<!--    }-->
+<!--    .not-found-btn-gohome {-->
+<!--      margin-left: 30px;-->
+<!--    }-->
+<!--  }-->
+<!--</style>-->
