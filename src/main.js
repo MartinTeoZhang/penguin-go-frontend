75ff77b507ae6fb231f56b288fbb744e8dbd1132
Ryzin <914571420@qq.com>
2020-02-02 22:14:54 +0800

    引入font-awesome图标库、自定义阿里iconfont库

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import api from './http/index' // 在 main.js 中以 vue 插件的形式引入 axios，
-// 这样在其他地方就可通过 this.$api 调用相关的接口了。
+import api from './http/index' // 在 main.js 中以 vue 插件的形式引入 axios，这样在其他地方就可通过 this.$api 调用相关的接口了。
 import i18n from './i18n'
 import store from './store'
-import 'font-awesome/css/font-awesome.min.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'font-awesome/css/font-awesome.min.css'
+import '@/assets/iconfont/iconfont.css'
 
 Vue.use(ElementUI) // 导入所有Element-UI组件
 Vue.use(api) // 导入 axios 插件
