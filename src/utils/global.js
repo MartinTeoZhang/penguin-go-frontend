b92c34a67994887586675f4b97fe22d735429f67
Ryzin <914571420@qq.com>
2020-04-05 14:19:34 +0800

    面包屑导航替换从标签页导航

--- src/utils/global.js
+++ src/utils/global.js
@@ -6,11 +6,11 @@
 
 // 后台管理系统服务器地址
 // export const baseUrl= 'http://192.168.83.131:8080/penguin-boot'
-export const baseUrl= 'http://localhost:8001/'
+export const baseUrl= 'http://localhost:8001'
 
 // 系统数据备份还原服务器地址
 // export const backupBaseUrl= 'http://192.168.83.131:8080/penguin-backup'
-export const backupBaseUrl= 'http://localhost:8002/'
+export const backupBaseUrl= 'http://localhost:8002'
 
 export default {
   baseUrl,
