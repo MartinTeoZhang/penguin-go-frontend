d6ffdc82826ad71ed23c6d55706fe689287d4050
Ryzin <914571420@qq.com>
2020-04-05 18:43:20 +0800

    fix bugs: 路由跳转问题
    路由跳转，需要加上根路径'/', 否则会认为是相对当前路由的路径，this..push(/ + path)

--- src/components/MenuTree/index.vue
+++ src/components/MenuTree/index.vue
@@ -32,8 +32,7 @@
           path = menu.url
         }
         // 通过菜单URL跳转至指定路由
-        this.$router.push("/")
-        this.$router.push(path)
+        this.$router.push("/" + path)
       }
     }
   }
