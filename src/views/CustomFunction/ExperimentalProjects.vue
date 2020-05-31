3a4b78281ca724e5245834038a8a1c205146e438
GuoYongcong <871352780@qq.com>
2020-05-31 23:40:36 +0800

    实验项目-报名功能

--- src/views/CustomFunction/ExperimentalProjects.vue
+++ src/views/CustomFunction/ExperimentalProjects.vue
@@ -20,12 +20,38 @@
         </el-form-item>
       </el-form>
     </div>
+    <!--报名参加界面-->
+
+    <el-dialog :title="'报名'" width="30%" :visible.sync="dialogVisible" :close-on-click-modal="false">
+      <div>
+        <span class="dialog-row">实验地点：{{clickedExp.location}}</span>
+      </div>
+      <!-- <span>实验时间段：</span><br />
+      <div v-for="(time,index) in timeArray" :key="index">
+        <span>{{time}}</span>
+      </div> -->
+      <div v-if="clickedExp.requirements">
+        <span class="dialog-row">实验要求：</span>
+        <span v-for="(r,index) in strToArray(clickedExp.requirements)" :key="index">
+          {{r}};
+        </span>
+      </div>
+      <div v-if="clickedExp.note">
+        <span class="dialog-row">备注：{{clickedExp.note}}</span>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button :size="size" @click.native="dialogVisible = false">{{$t('action.cancel')}}</el-button>
+        <el-button :size="size" type="primary" @click.native="submitForm" :loading="editLoading">报名参加</el-button>
+      </div>
+    </el-dialog>
+
+    <!--展示实验项目-->
     <div class="cards" >
       <!-- Card卡片 -->
       <!-- <el-card shadow="hover" class="box-card">
         <div slot="header" class="clearfix">
           <span>简单的按键+皮电实验</span>
-          <sapn style="float: right; color: red" type="text">25元</sapn>
+          <span style="float: right; color: red" type="text">25元</span>
         </div>
         <el-row :gutter="20">
           <el-col :span="11">
@@ -39,10 +65,10 @@
           </el-col>
         </el-row>
       </el-card> -->
-      <el-card shadow="hover" class="box-card" v-for="(re, index) in pageResult.content" :key="index">
+      <el-card shadow="hover" class="box-card" v-for="(re, index) in pageResult.content" :key="index" @click.native="clickCard(re, index)">
         <div slot="header" class="clearfix">
           <span>{{re.name}}</span>
-          <sapn style="float: right; color: red" type="text">{{getPaymentMin(re.payment)}}~{{getPaymentMax(re.payment)}}元</sapn>
+          <span style="float: right; color: red" type="text">{{payToStr(re.payment)}}元</span>
         </div>
         <el-row :gutter="20">
           <el-col :span="11">
@@ -51,7 +77,7 @@
           </el-col>
           <el-col :span="9">
             <div class="text item">
-              人数：{{re.peopleNum}}人
+              人数：{{expUserCount[index]}}/{{re.peopleNum}}人
             </div>
             <div class="text item">
               时长：{{re.duration}}分钟
@@ -100,6 +126,18 @@
         },
         pageRequest: { pageNum: 1, pageSize: 10 },
         pageResult: {},
+        dialogVisible: false, // 报名参加界面是否显示
+        editLoading: false,
+        clickedExp: { //被点击的实验
+          types: [],
+          location: '',
+          time: null,
+          duration: 1,
+          requirements: null,
+          note: '',
+        },
+        userId: 0,
+        expUserCount: [], //实验报名人数
       };
     },
     mounted(){
@@ -114,6 +152,8 @@
         this.pageRequest.columnFilters = {status: {name:'status', value:'2'}}
         this.$api.exp.findPage(this.pageRequest).then((res) => {
           this.pageResult = res.data
+          this.expUserCount = []
+          this.getExpUserCount()
         })
       },
       handleAdd: function () {
@@ -128,17 +168,130 @@
         str = str.substring(1,str.length-1);
         return str.split(",");
       },
-      getPaymentMin: function(payment){
-        return this.strToJson(payment).paymentMin
+      payToStr: function(pay){
+        let p = this.strToJson(pay)
+        return p.paymentMin + '~' + p.paymentMax
       },
-      getPaymentMax: function(payment){
-        return this.strToJson(payment).paymentMax
+      getExpUserCount: function(){
+        let len = this.expUserCount.length
+        if(len < this.pageResult.content.length){
+          let request = {expId: this.pageResult.content[len].id}
+          this.$api.exp.getExpUserCount(request).then((res) => {
+            this.expUserCount.push(res.data)
+            this.getExpUserCount()
+          })
+        }
+      },
+      //更新实验的报名人数
+      getExpUserCountByExpId: function(){
+        let request = {expId: this.clickedExp.id}
+        this.$api.exp.getExpUserCount(request).then((res) => {
+          this.expUserCount.splice(this.clickedExpIndex, 1, res.data)
+        })
       },
       // 换页刷新
       refreshPageRequest: function (pageNum) {
         this.pageRequest.pageNum = pageNum
         this.findPage(null)
       },
+      submitForm: function () {
+        this.$confirm('确认提交吗？', '提示', {}).then(() => {
+          if(this.isFull()){
+            this.$message({message: '报名人数已满！', type: 'warning'})
+            return
+          }
+          this.editLoading = true
+          if(this.userId == 0 || this.userId == null){
+            //通过userName获得userId
+            let userName = sessionStorage.getItem('user')
+            var request = { pageNum: 1, pageSize: 10 }
+            request.columnFilters = {name: {name:'name', value: userName}}
+            this.$api.user.findPage(request).then((res) => {
+              this.userId = res.data.content[0].id
+              this.verifyRepeatedEnrollment()
+            })
+          }else{
+            this.verifyRepeatedEnrollment()
+          }
+        })
+      },
+      clickCard: function(exp, index){
+        this.dialogVisible = true
+        this.clickedExp = exp
+        this.clickedExpIndex = index
+      },
+      saveExpUser: function(){
+        let expUser = {id: 0, expId: this.clickedExp.id, userId: this.userId}
+        this.$api.exp.saveExpUser(expUser).then((res) => {
+          this.editLoading = false
+          if(res.code == 200) {
+            this.$message({ message: '操作成功', type: 'success' })
+            this.dialogVisible = false
+            this.getExpUserCountByExpId()
+          } else {
+            this.$message({message: '操作失败, ' + res.msg, type: 'error'})
+          }
+        })
+      },
+      //验证重复报名
+      verifyRepeatedEnrollment : function(){
+        let request = {expId: this.clickedExp.id}
+        this.$api.exp.findExpUsers(request).then((res) => {
+          for (var i = res.data.length - 1; i >= 0; i--) {
+            if(res.data[i].userId == this.userId){
+              this.editLoading = false
+              this.$message({message: '无法重复报名', type: 'warning'})
+              return
+            }
+          }
+          this.saveExpUser()
+        })
+      },
+      //判断报名人数是否满了
+      isFull: function(){
+        return this.expUserCount[this.clickedExpIndex] >= this.clickedExp.peopleNum
+      }
+    },
+    computed:{
+      timeArray: function(){
+        if(this.clickedExp.time === null)
+          return null
+        console.log(this.clickedExp.time)
+        var e = this.strToJson('exp:'+this.clickedExp.time)
+        console.log(e)
+        var arr = this.strToArray(this.clickedExp.time)
+        console.log(arr)
+        var t
+        var timeArr = []
+        Date.prototype.format = function(fmt) {
+          var o = {
+            "M+" : this.getMonth()+1,                 //月份
+            "d+" : this.getDate(),                    //日
+            "h+" : this.getHours(),                   //小时
+            "m+" : this.getMinutes(),                 //分
+            "s+" : this.getSeconds(),                 //秒
+            "q+" : Math.floor((this.getMonth()+3)/3), //季度
+            "S"  : this.getMilliseconds()             //毫秒
+          };
+          if(/(y+)/.test(fmt)) {
+            fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+          }
+          for(var k in o) {
+            if(new RegExp("("+ k +")").test(fmt)){
+              fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+            }
+          }
+          return fmt;
+        }
+        for(t in arr){
+          var str = new Date(t.dateValue).format("yyyy-MM-dd")
+          console.log(t)
+          str = str + " " + new Date(t.timeValue[0]).format("hh:mm")
+          str = str + "至" + new Date(t.timeValue[1]).format("hh:mm")
+          timeArr.push(str)
+        }
+        return timeArr
+      }
     }
   }
 </script>
@@ -165,6 +318,10 @@
   .box-card {
     width: 500px;
     height: 200px;
+    text-align: left;
+  }
+
+  .el-dialog {
     text-align: left;
   }
 
